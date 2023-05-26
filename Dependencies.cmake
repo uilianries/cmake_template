1e9dcbff698a3fc7c601721645e437a3131b2bea
Uilian Ries <uilianries@gmail.com>
2023-05-26 11:17:59 +0200

    use conan
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- Dependencies.cmake
+++
@@ -1,9 +0,0 @@
-include(cmake/Conan.cmake)
-
-conan_install_conanfile(${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt)
-
-find_package(fmt CONFIG REQUIRED)
-find_package(spdlog CONFIG REQUIRED)
-find_package(Catch2 CONFIG REQUIRED)
-find_package(CLI11 CONFIG REQUIRED)
-find_package(ftxui CONFIG   REQUIRED)
